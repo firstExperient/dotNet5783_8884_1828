2a981baeea07d77d7843d1e19b7a092bba2112c6
w0583233625 <w0583233625@gmail.com>
2022-09-19 23:01:03 +0300

    Inital code + Welcome functions

---
+++ Stage0/Program1828.cs
@@ -0,0 +1,16 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome1828();
+        Console.ReadKey();
+    }
+
+    static partial void welcome8884();
+    private static void welcome1828()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
