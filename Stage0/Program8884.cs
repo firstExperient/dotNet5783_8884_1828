89722fb56d556d3c28447a23e591db75392650d4
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2022-09-19 23:56:24 +0300

    another welcome function

---
+++ Stage0/Program8884.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    internal partial class Program
+    {
+        private static void welcome8884()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
