f80f060dedafbb5966f9a8fd9c835cb2562a456b
w0583233625 <w0583233625@gmail.com>
2022-11-01 16:01:11 +0200

    adding to datasource class

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -4,6 +4,7 @@ namespace DO;
 
 public struct OrderItem
 {
+    public int ID { get; set; }
     public int ProductId { get; set; }
     public int OrderId { get; set; }
     public double Price { get; set; }
--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -2,18 +2,108 @@
 
 namespace Dal;
 
-public struct DataSource
+public static class DataSource
 {
-    internal static Product[] Products;
-    internal static Order[] Orders;
-    internal static OrderItem[] OrderItems;
-    internal struct Config
+    static DataSource()
     {
-        static internal int ProductsIndex = 0;
-        static internal int OrdersIndex = 0;
-        static internal int OrderItemsIndex = 0;
-
+        s_Initialize();
     }
 
-    internal static void Initialize() { }
+
+    #region entities' array
+
+    internal static Product[] Products = new Product[50];
+
+    private static void AddProduct(string name, Category category,double price, int inStock) {
+        int id = Random.Next(100000, 1000000);
+
+        //add here a check to see that the random id doesnt already exist
+
+        Products[Config.ProductsIndex] = new Product()
+        {
+            ID = id,
+            Name = name,
+            Category = category,
+            Price = price,
+            InStock = inStock
+        };
+
+        Config.ProductsIndex++;
+    }
+
+    internal static Order[] Orders = new Order[100];
+    
+    private static void AddOrder(string customerName,string customerEmail,string customerAdress,DateTime orderDate,DateTime shipDate,DateTime deliveryDate) {
+        int id = Config.OrderId;
+
+        Orders[Config.OrdersIndex] = new Order()
+        {
+            ID = id,
+            CustomerName = customerName,
+            CustomerEmail = customerEmail,
+            CustomerAdress = customerAdress,
+            OrderDate = orderDate,
+            ShipDate = shipDate,
+            DeliveryDate = deliveryDate
+        };
+
+        Config.OrdersIndex++;
+    }
+
+    internal static OrderItem[] OrderItems = new OrderItem[200];
+    private static void AddOrderItem(int productId,int orderId,double price,int amount) {
+        int id = Config.OrderItemId;
+
+        OrderItems[Config.OrderItemsIndex] = new OrderItem()
+        {
+            ID = id,
+            ProductId = productId,
+            OrderId = orderId,
+            Price = price,
+            Amount = amount
+        };
+
+        Config.OrderItemsIndex++; 
+    }
+
+    #endregion
+
+    internal static readonly Random Random = new Random();
+
+    #region config
+        internal class Config
+        {
+
+            static internal int ProductsIndex = 0;
+            static internal int OrdersIndex = 0;
+            static internal int OrderItemsIndex = 0;
+
+            static private int orderId = 0;//check if 0 is the begining
+            static internal int OrderId
+            {
+                get
+                {
+                    orderId++;
+                    return orderId;
+                }
+            }
+
+            static private int orderItemId = 0;//check this too
+
+            static internal int OrderItemId
+            {
+                get
+                {
+                    orderItemId++;
+                    return orderItemId;
+                }
+            }
+        }
+
+    #endregion
+
+    #region initialize
+        private static void s_Initialize() { }
+
+    #endregion
 }
