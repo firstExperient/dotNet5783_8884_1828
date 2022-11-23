38785e58ba085bb7817ae7a9219b6ad3bdbd88f7
w0583233625 <w0583233625@gmail.com>
2022-11-23 21:44:07 +0200

    adding to cart

--- BL/BlImplementation/Cart.cs
+++ BL/BlImplementation/Cart.cs
@@ -4,51 +4,102 @@ using DO;
 
 namespace BlImplementation;
 
-internal class Cart: ICart // fix this (I dont know why this is error)
+internal class Cart: ICart 
 {
     private DalApi.IDal Dal = new DalList();
 
     #region ADD
 
-    public Cart AddItem(int id, BO.Cart cart)
+    public BO.Cart AddItem(int id, BO.Cart cart)
     {
         try
         {
-            DO.Product dalProduct = Dal.Product.Get(id);
-            
-          foreach  (BO.Product item in cart.Items)
+            DO.Product product = Dal.Product.Get(id);
+
+            for(int i = 0; i < cart.Items.Count; i++)
             {
-
+                if(cart.Items[i].ID == id)//the product is already in cart
+                {
+                  //fix this
+                  //האם הכמות היא לפי הכמות הכללית פחות מה שכבר יש או רק פחות האחד שהוא מוסיף?
+                }
             }
-
-            if (dalProduct)
+            //the product is not in cart
+            if(product.InStock <= 0)
+                throw new Exception();//fix this
+            //fix this - what to do with the id
+            cart.Items.Add(new BO.OrderItem()
             {
-                
-            cart.TotalPrice += dalProduct.Price;
-           
-            dalProduct.Price=
-            }
-            else
-            {
-
-            }
-
+                ProductId = id,
+                Name = product.Name,
+                Price = product.Price,
+                Amount = 1,
+                TotalPrice = product.Price,
+            });
+            cart.TotalPrice += product.Price;
+            return cart;
         }
         catch (Exception)
         {
             //fix this
             throw;
         }
-        throw new NotImplementedException();
     }
 
     #endregion
-    public Cart UpdateItemAmount(int id, Cart cart, int newAmount)
+
+    #region update item amount
+
+    public BO.Cart UpdateItemAmount(int id, BO.Cart cart, int newAmount)
     {
 
     }
-    public void ConfirmOrder(Cart cart, string customerName, string email, string adress)
-    {
 
+    #endregion
+
+    #region confirm order
+
+    public void ConfirmOrder(BO.Cart cart, string customerName, string email, string adress)
+    {
+        //fix this - add validation checks
+
+        int orderId = Dal.Order.Add(new DO.Order()//add the order to database
+        {
+            //no need to add id - auto id is generete
+            CustomerName = customerName,
+            CustomerEmail = email,
+            CustomerAdress = adress,
+            OrderDate = DateTime.Now,
+            ShipDate = DateTime.MinValue,
+            DeliveryDate = DateTime.MinValue,
+        });
+        foreach (var item in cart.Items)//add each item 
+        {
+            DO.Product product;
+            try
+            {
+                product = Dal.Product.Get(item.ProductId);
+                if (product.InStock < item.Amount)
+                    throw new Exception();//fix this
+                product.InStock -= item.Amount;
+                Dal.OrderItem.Add(new OrderItem()
+                {
+                    //no need to add id - auto id is generete
+                    OrderId = orderId,
+                    ProductId = item.ProductId,
+                    Amount = item.Amount,
+                    Price = item.Price,
+                });
+                Dal.Product.Update(product);//update the amount of product in stock
+            }
+            catch (Exception)
+            {
+                //fix this
+                throw;
+            }
+        }
     }
+
+    #endregion
+
 }
