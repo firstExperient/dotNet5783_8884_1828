b163b9b9a5e96c2fefcab1ff663999c2ab8eeb29
w0583233625 <w0583233625@gmail.com>
2022-11-23 20:31:49 +0200

    starting with the bltest, and adding to the product

---
+++ BL/BlImplementation/Bl.cs
@@ -0,0 +1,13 @@
+﻿using BlApi;
+using System.Security.Principal;
+
+
+namespace BlImplementation;
+
+sealed public class Bl:IBl
+{
+    public IProduct Product => new Product();
+    public ICart Cart => new BoEntity();
+    public IOrder Order => new BoEntity();
+
+}
