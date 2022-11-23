a3a394e68f7bdbe3f09df081e88a749665afb85d
w0583233625 <w0583233625@gmail.com>
2022-11-23 18:53:00 +0200

    edditing the blapi

---
+++ BL/BlApi/IBl.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace BlApi;
+
+public interface IBl
+{
+    public IProduct Product { get; }
+    public ICart Cart { get; }
+    public IOrder Order { get; }
+}
--- BL/BlApi/IOrder.cs
+++ BL/BlApi/IOrder.cs
@@ -8,4 +8,8 @@ public interface IOrder
     public IEnumerable<OrderForList> GetAll();
     public Order Get(int id);
 
+    public Order ShipOrder(int id);
+    public Order DeliverOrder(int id);
+    public OrderTracking TrackOrder(int id);
+    public void UpdateOrder(Order order);//for bonus - check this
 }
