52c37f377eda4f1ac27abc68668b3752e350c20f
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2022-11-23 20:32:48 +0200

    cart in BO

---
+++ BL/BlImplementation/Cart.cs
@@ -0,0 +1,54 @@
+﻿using BlApi;
+using Dal;
+using DO;
+
+namespace BlImplementation;
+
+internal class Cart: ICart // fix this (I dont know why this is error)
+{
+    private DalApi.IDal Dal = new DalList();
+
+    #region ADD
+
+    public Cart AddItem(int id, BO.Cart cart)
+    {
+        try
+        {
+            DO.Product dalProduct = Dal.Product.Get(id);
+            
+          foreach  (BO.Product item in cart.Items)
+            {
+
+            }
+
+            if (dalProduct)
+            {
+                
+            cart.TotalPrice += dalProduct.Price;
+           
+            dalProduct.Price=
+            }
+            else
+            {
+
+            }
+
+        }
+        catch (Exception)
+        {
+            //fix this
+            throw;
+        }
+        throw new NotImplementedException();
+    }
+
+    #endregion
+    public Cart UpdateItemAmount(int id, Cart cart, int newAmount)
+    {
+
+    }
+    public void ConfirmOrder(Cart cart, string customerName, string email, string adress)
+    {
+
+    }
+}
