74196539a191f4a43428095308acf0b970297818
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2022-12-13 17:16:52 +0200

    addProduct_Click function

--- PL/Products/ProductWindow.xaml.cs
+++ PL/Products/ProductWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using System.Reflection.Emit;
+using System;
+using System.Reflection.Metadata;
+using System.Windows;
+using System.Windows.Controls;
+using BlApi;
+using BlImplementation;
 
 namespace PL.Products;
 
@@ -7,8 +13,31 @@ namespace PL.Products;
 /// </summary>
 public partial class ProductWindow : Window
 {
+    private IBl bl = new Bl();
+    
+
     public ProductWindow()
     {
         InitializeComponent();
+    }
+
+/*    private void ID_TextChanged(object sender, EventArgs e)
+    {
+ 
+    }*/
+
+
+    private void addProduct_Click(object sender, RoutedEventArgs e)
+    {
+        // MessageBox.Show(id_input.Text);
+
+        bl.Product.Add(new Product()
+        {
+            ID = Int32.Parse(id_input.Text),
+            Name = name_input.Text,
+            Price = Int32.Parse(price_input.Text),
+            Category = "TODO: ",
+            InStock = Int32.Parse(inStock_input.Text),
+        });
     }
 }
