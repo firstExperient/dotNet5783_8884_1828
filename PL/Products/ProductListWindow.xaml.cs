508e76de038150a735a36f54a9d810bbcc6ac1ba
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2022-12-13 13:56:48 +0200

    add product page

--- PL/Products/ProductListWindow.xaml.cs
+++ PL/Products/ProductListWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using BlApi;
 using BlImplementation;
+using BO;
 using System;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,14 +22,19 @@ public partial class ProductListWindow : Window
 
     void CategorySelectionChanged(object sender, SelectionChangedEventArgs args)
     {
+        // TODO: call get-products function from BL filtered by category and display
+
+        
         string selection = ComboBox.SelectedItemProperty.ToString();
+        MessageBox.Show(ComboBox.DataContextProperty.ToString());
 
-        // TODO: call get-products function from BL filtered by category
-
-        MessageBox.Show("selection :" + selection);
+        InitializeComponent();
+        ProductsListview.ItemsSource = bl.Product.GetByCategory(selection.ToString());
+        CategorySelector.ItemsSource = Enum.GetValues(typeof(BO.Category));
     }
+
     private void AddNewProductButton_Click(object sender, RoutedEventArgs e)
     {
-        //new ProductListWindow().Show();
+        new ProductWindow().Show();
     }
 }
