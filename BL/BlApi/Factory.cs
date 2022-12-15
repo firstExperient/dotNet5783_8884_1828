6a630bb3348c50c953dff9fcda39189b1199018f
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2022-12-15 12:15:57 +0200

    Factory class in BL

---
+++ BL/BlApi/Factory.cs
@@ -0,0 +1,12 @@
+﻿using BlImplementation;
+namespace BlApi
+
+{
+    public class Factory
+    {
+        public static IBl Get() {
+            BlImplementation.Bl bl = new Bl();
+            return bl;
+        }
+    }
+}
