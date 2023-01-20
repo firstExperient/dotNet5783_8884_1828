060298c1cff6c9c0ace899a2eba2d1784e5d3fe3
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2023-01-20 11:19:35 +0200

    summery

--- DalXml/FilesManage.cs
+++ DalXml/FilesManage.cs
@@ -1,10 +1,17 @@
-﻿
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
 
 namespace Dal;
 
 public class FilesManage
 {
+
+    /// <summary>
+    /// This function is to read an XML file whos path is the path
+    /// given as parameter return the data in the file in a list form.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns>list of data</returns>
+
     static public List<T> ReadList<T>(string path)
     {
         XmlSerializer xs = new XmlSerializer(typeof(List<T>));
@@ -13,6 +20,12 @@ public class FilesManage
         sr.Close();
     }
 
+    /// <summary>
+    /// This function is to save in a file whos path is the path
+    /// given as parameter the data in the list given in an XML form.
+    /// </summary>
+    /// <param name="list">list of data</param>
+    /// <param name="path">path of the XML file</param>
     static public void SaveList<T>(List<T> list, string path)
     {
         XmlSerializer xs = new XmlSerializer(list.GetType());
