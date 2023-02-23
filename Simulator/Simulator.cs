c1dc19d2d8b8675d7df6b90797c3bd0b1bc2b7c8
w0583233625 <w0583233625@gmail.com>
2023-02-24 00:54:02 +0200

    fixing Sinulator

--- Simulator/Simulator.cs
+++ Simulator/Simulator.cs
@@ -1,21 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Simulator
+namespace Simulator;
+
+internal static class Simulator
 {
-    internal static class Simulator
+    public static void startSimulation()
     {
-        public static void startSimulation()
-        {
 
-        }
+    }
 
-        public static void stopSimulation()
-        {
+    public static void stopSimulation()
+    {
 
-        }
     }
 }
