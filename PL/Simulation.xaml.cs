09f18c656523fb4d0c62b2f19975c15a80c70680
Einav Esther Chaya Borg <eiinavborg@gmail.com>
2023-02-25 21:35:18 +0200

    Timer

--- PL/Simulation.xaml.cs
+++ PL/Simulation.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using System.Windows;
+using System.Timers;
+//using System.Threading;
 
 namespace PL
 {
@@ -19,14 +11,69 @@ namespace PL
     /// </summary>
     public partial class Simulation : Window
     {
+        Timer t = new Timer();
         public Simulation()
         {
             InitializeComponent();
         }
-     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //timer interval
+            t.Interval = 1000;  //in milliseconds
+
+            t.Elapsed += (sender, e) => t_Tick(sender, e);
+
+            //start timer when form loads
+            t.Start();  //this will use t_Tick() method
+        }
+
+        private void t_Tick(object sender, EventArgs e)
+        {
+            //get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            //time
+            string time = "";
+
+            //padding leading zero
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+
+            //update label
+            clock.Text = time;
+        }
+
         public void StopSimulation(object sender, RoutedEventArgs e)
         {
-
+            t.Dispose();
         }
     }
 }
